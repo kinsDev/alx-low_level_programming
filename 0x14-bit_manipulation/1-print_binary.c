8f2acd7e362cbb0490247f016f0dba9344826476
kinsDev <kinsleygitonga@gmail.com>
2023-07-06 17:55:18 -0700

    function to print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,49 @@
-#include <stdio.h>
 #include "main.h"
 
+#include "main.h"
+
+/**
+ * power - calculates the result of a number raised to a power
+ * @base: the base number
+ * @exponent: the exponent
+ * Return: the result of base raised to the exponent
+ */
+unsigned long int power(unsigned int base, unsigned int exponent)
+{
+	unsigned long int result = 1;
+	unsigned int i;
+
+	for (i = 1; i <= exponent; i++)
+		result *= base;
+
+	return result;
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to be printed in binary
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	if(n > 1)
-	{
-		print_binary(n / 2);
-	}
+	unsigned long int divisor, result;
+	char flag = 0;
 
-	printf("%lu", n % 2);
+	divisor = power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (divisor != 0)
+	{
+		result = n & divisor;
+		if (result == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
 }
+
