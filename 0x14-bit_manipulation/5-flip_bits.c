857363c378f6c740ee249581be21360c63ea2b81
kinsDev <kinsleygitonga@gmail.com>
2023-07-06 19:11:29 -0700

    a function that returns the number of bits you would need to flip to get from one number to another.

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,27 @@
 #include "main.h"
-
 /**
- * flip_bits - takes argument a & b as inputs+
- * Initialize count to 0 nd keep track of the number of bits that need to be flipped
- * Calculate the XOR (exclusive OR) of a and b using the bitwise XOR operator ^.
+ * flip_bits - Returns the number of bits you would need to flip to get from one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits you would need to flip.
  */
-
-int flip_bits(unsigned long int a, unsigned long int b)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor_result = a ^ b;
-    int count = 0;
+    unsigned long int difference = n ^ m; // Calculate the XOR difference between the two numbers.
+    unsigned int count = 0; // Initialize the count of flipped bits.
+    unsigned int i;
 
-    while (xor_result != 0) {
-        count += xor_result & 1;
-        xor_result >>= 1;
+    // Iterate over each bit position in the difference.
+    for (i = 0; i < sizeof(unsigned long int) * 8; i++) {
+        // If the current bit is set (i.e., different between the numbers),
+        // increment the count of flipped bits.
+        if ((difference & 1) == 1) {
+            count++;
+        }
+        difference >>= 1; // Shift the difference to the right to check the next bit.
     }
 
-    return count;
+    return count; // Return the count of flipped bits.
 }
 
