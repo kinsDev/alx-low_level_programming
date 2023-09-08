63a2283f94576cd2b34c499efa948808c2dce2ec
kinsDev <kinsleygitonga@gmail.com>
2023-09-08 15:18:40 -0700

    a hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,20 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * hash_djb2 - This function implements the djb2 hashing algorithm.
+ * @str: The string used to generate the hash value.
  *
- * Return: hash value
+ * Return: The calculated hash value.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
-	int c;
+    unsigned long int hash; /* Initialize the hash variable. */
+    int c; /* Variable to hold the current character being processed. */
 
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+    hash = 5381; /* Initialize the hash with a prime number. */
+    while ((c = *str++)) /* Loop through each character in the input string. */
+    {
+        /* Update the hash using the djb2 formula: hash * 33 + c */
+        hash = ((hash << 5) + hash) + c;
+    }
+    return (hash); /* Return the calculated hash value. */
 }
